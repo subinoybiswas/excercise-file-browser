3052cf21fecb4f601afb0b2e7e479aa87ed2205e
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-07-16 18:38:22 +0530

    remove: transparent bg

--- components/file.tsx
+++ components/file.tsx
@@ -2,7 +2,7 @@ import { FileType } from "@/types/FileType";
 import { File as FileIcon } from "lucide-react"
 export function File({ file }: { file: FileType }) {
     return (
-        <div className="flex flex-row items-center gap-1 p-2 bg-slate-50 hover:bg-blue-400/40 cursor-default">
+        <div className="flex flex-row items-center gap-1 p-2 bg-slate-50 hover:bg-blue-200 cursor-default">
             <FileIcon />
             {file.name}
         </div>
